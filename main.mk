c533ac0eb4796bf94b3cb95cdbb9d7b1e8eec827
Rosiney Gomes Pereira <rosiney.gp@gmail.com>
2020-02-16 22:26:52 -0300

    feat: create base jobs and templates

--- main.mk
+++ main.mk
@@ -1,7 +1,25 @@
 MKDKR_EXPORT_TAG=latest
 
-export.gitlab-ci:
+define export_header =
 	@$(.)
 	... rosiney/mkdkr_exporter:$(MKDKR_EXPORT_TAG)
-	.. /app/export_data > export.yaml
-	.. jinja2 --format=yml /app/templates/.gitlab-ci.yml.j2 export.yaml > .gitlab-ci.yml
+	.. /app/export_data \> /tmp/export.yaml
+endef
+
+export.gitlab-ci:
+	$(export_header)
+	.. jinja2 --format=yml /app/templates/gitlab-ci.yml.j2 /tmp/export.yaml > .gitlab-ci.yml
+
+export.github:
+	$(export_header)
+	mkdir -p .github/workflows/
+	.. jinja2 --format=yml /app/templates/github.yml.j2 /tmp/export.yaml > .github/workflows/main.yml
+
+export.travis:
+	$(export_header)
+	.. jinja2 --format=yml /app/templates/travis.yml.j2 /tmp/export.yaml > .travis.yml
+
+export.circle-ci:
+	$(export_header)
+	mkdir -p .circleci
+	.. jinja2 --format=yml /app/templates/travis.yml.j2 /tmp/export.yaml > .circleci/config.yml
