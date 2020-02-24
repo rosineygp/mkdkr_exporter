45794dc915177e8a791f23deb428097f3cb7babd
Rosiney Gomes Pereira <rosiney.gp@gmail.com>
2020-02-24 09:31:48 -0300

    chore: update mkdkr to newest

--- main.mk
+++ main.mk
@@ -1,29 +1,29 @@
 MKDKR_EXPORTER_TAG=latest
 
 define exporter_header =
-	@$(.)
-	... rosiney/mkdkr_exporter:$(MKDKR_EXPORTER_TAG)
-	.. /app/export_data \> /tmp/export.yaml
+	@$(dkr)
+	instance: rosiney/mkdkr_exporter:$(MKDKR_EXPORTER_TAG)
+	run: /app/export_data \> /tmp/export.yaml
 endef
 
 _exporter_gitlab-ci:
 	$(exporter_header)
-	.. jinja2 --format=yml /app/templates/gitlab-ci.yml.j2 /tmp/export.yaml > .gitlab-ci.yml
+	run: jinja2 --format=yml /app/templates/gitlab-ci.yml.j2 /tmp/export.yaml > .gitlab-ci.yml
 
 _exporter_github:
 	$(exporter_header)
 	mkdir -p .github/workflows/
-	.. jinja2 --format=yml /app/templates/github.yml.j2 /tmp/export.yaml > .github/workflows/main.yml
+	run: jinja2 --format=yml /app/templates/github.yml.j2 /tmp/export.yaml > .github/workflows/main.yml
 
 _exporter_travis:
 	$(exporter_header)
-	.. jinja2 --format=yml /app/templates/travis.yml.j2 /tmp/export.yaml > .travis.yml
+	run: jinja2 --format=yml /app/templates/travis.yml.j2 /tmp/export.yaml > .travis.yml
 
 _exporter_circle-ci:
 	$(exporter_header)
 	mkdir -p .circleci
-	.. jinja2 --format=yml /app/templates/travis.yml.j2 /tmp/export.yaml > .circleci/config.yml
+	run: jinja2 --format=yml /app/templates/travis.yml.j2 /tmp/export.yaml > .circleci/config.yml
 
 _exporter_jenkins_pipeline:
 	$(exporter_header)
-	.. jinja2 --format=yml /app/templates/Jenkinsfile.j2 /tmp/export.yaml > Jenkinsfile
+	run: jinja2 --format=yml /app/templates/Jenkinsfile.j2 /tmp/export.yaml > Jenkinsfile
