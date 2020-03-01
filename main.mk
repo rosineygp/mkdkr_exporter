f770325f517a89962cf5919c5e15f6938f9c007c
Rosiney Gomes Pereira <rosiney.gp@gmail.com>
2020-03-01 19:34:38 -0300

    feat: github parallel job schedule

--- main.mk
+++ main.mk
@@ -1,29 +1,29 @@
 MKDKR_EXPORTER_TAG=latest
 
-define exporter_header =
+define _exporter_header =
 	@$(dkr)
 	instance: rosiney/mkdkr_exporter:$(MKDKR_EXPORTER_TAG)
 	run: /app/export_data \> /tmp/export.yaml
 endef
 
 _exporter_gitlab-ci:
-	$(exporter_header)
+	$(call _exporter_header)
 	run: jinja2 --format=yml /app/templates/gitlab-ci.yml.j2 /tmp/export.yaml > .gitlab-ci.yml
 
 _exporter_github:
-	$(exporter_header)
+	$(call _exporter_header)
 	mkdir -p .github/workflows/
 	run: jinja2 --format=yml /app/templates/github.yml.j2 /tmp/export.yaml > .github/workflows/main.yml
 
 _exporter_travis:
-	$(exporter_header)
+	$(call _exporter_header)
 	run: jinja2 --format=yml /app/templates/travis.yml.j2 /tmp/export.yaml > .travis.yml
 
 _exporter_circle-ci:
-	$(exporter_header)
+	$(call _exporter_header)
 	mkdir -p .circleci
 	run: jinja2 --format=yml /app/templates/travis.yml.j2 /tmp/export.yaml > .circleci/config.yml
 
 _exporter_jenkins_pipeline:
-	$(exporter_header)
+	$(call _exporter_header)
 	run: jinja2 --format=yml /app/templates/Jenkinsfile.j2 /tmp/export.yaml > Jenkinsfile
